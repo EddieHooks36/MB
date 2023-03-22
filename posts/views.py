eedab1890887d07bb17e2de714513a8ff5609073
EddieHooks36 <eddiehooks36@gmail.com>
2023-03-21 20:13:51 -0700

    Post update

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,7 @@
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Post
+
 
 
 class PostListView(ListView):
@@ -12,6 +13,15 @@ class PostDetailView(DetailView):
     model = Post
 
 class PostCreateView(CreateView):
-    template_name = "posts/create.html"
+    template_name = "posts/new.html"
     model = Post
-    fields = ["title", "subtitle", "body"]
+    fields = ["title", "subtitle", "body", "author"]
+
+class PostDeleteVIew(DeleteView):
+    template_name = "posts/delete.html"
+    model = Post
+
+class PostUpdateView(UpdateView):
+    template_name = "posts/update.html"
+    model = Post
+    fields = ["table", "subtitle", "body"]
