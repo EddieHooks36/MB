6fc5d64e1679847c59f2df70d85ccea5c0422465
EddieHooks36 <eddiehooks36@gmail.com>
2023-03-26 12:05:35 -0700

    published

--- posts/urls.py
+++ posts/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView  #PostDeleteView
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, DraftPostListView
 
 urlpatterns = [
     path("",PostListView.as_view(), name="list"),
+    path("drafts/", DraftPostListView.as_view(), name="draft_posts"),
     path("<int:pk>/", PostDetailView.as_view(), name="detail"),
     path ("new/",PostCreateView.as_view(), name="new"),
     path("<int:pk>/edit/",PostUpdateView.as_view(), name="update"),
-    # path("<int:pk>/delete/",PostDeleteView.as_view(), name="delete"),
+    path("<int:pk>/delete/",PostDeleteView.as_view(), name="delete"),
 ]
 
